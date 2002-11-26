0221e481202a6f4902c501ca0fe84f736eabda84
Kevin R. Long <kevin.long@ttu.edu>
2002-11-26 06:28:31 +0000

    ignore this -- I'm trying to diagnose a problem with CVS hanging

--- packages/TSF/src/Support/TSFMPI.h
+++ packages/TSF/src/Support/TSFMPI.h
@@ -1,35 +1,35 @@
 #ifndef TSFMPI_H
 #define TSFMPI_H
 
-#include "TSFConfig.h"
+#include "TSFDefs.h"
 
-#if HAVE_MPI
+#ifdef HAVE_MPI
 #include "mpi.h"
 #endif
 
 namespace TSF
 {
-	/**
-	 *
-	 */
-	class TSFMPI
-		{
-		public:
-			/** initializer, calls MPI_Init() if necessary */
-			static void init(int* argc, void*** argv);
+  /**
+   *
+   */
+  class TSFMPI
+    {
+    public:
+      /** initializer, calls MPI_Init() if necessary */
+      static void init(int* argc, void*** argv);
 
-			/** returns the process rank relative to MPI_COMM_WORLD */
-			static int getRank() {return rank_;}
+      /** returns the process rank relative to MPI_COMM_WORLD */
+      static int getRank() {return rank_;}
 
-			/** returns the number of processors in MPI_COMM_WORLD */
-			static int getNProc() {return nProc_;}
+      /** returns the number of processors in MPI_COMM_WORLD */
+      static int getNProc() {return nProc_;}
 
-			/** finalizer, calls MPI_Finalize() if necessary */
-			static void finalize();
-		private:
-			static int rank_;
-			static int nProc_;
-		};
+      /** finalizer, calls MPI_Finalize() if necessary */
+      static void finalize();
+    private:
+      static int rank_;
+      static int nProc_;
+    };
 }
 
 #endif
