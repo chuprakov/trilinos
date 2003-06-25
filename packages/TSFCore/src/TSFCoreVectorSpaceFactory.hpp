4faa8ac895984521e50724c86f67edf9da8100e9
James M. Willenbring <jmwille@sandia.gov>
2003-06-25 21:33:36 +0000

    ----------------------------------------------------------------------
    Committing in .
    Removed Files:
    	TSFCoreAssertOp.hpp TSFCoreAssertOpDecl.hpp TSFCoreConfig.h.in
    	TSFCoreExplInst.cpp TSFCoreLinearOp.hpp
    	TSFCoreLinearOpDecl.hpp TSFCoreMultiVector.hpp
    	TSFCoreMultiVectorCols.hpp TSFCoreMultiVectorColsDecl.hpp
    	TSFCoreMultiVectorDecl.hpp TSFCoreMultiVectorStdOps.hpp
    	TSFCoreMultiVectorStdOpsDecl.hpp TSFCoreOpBase.hpp
    	TSFCoreOpBaseDecl.hpp TSFCoreSerialVector.hpp
    	TSFCoreSerialVectorBase.hpp TSFCoreSerialVectorBaseDecl.hpp
    	TSFCoreSerialVectorDecl.hpp TSFCoreSerialVectorSpace.hpp
    	TSFCoreSerialVectorSpaceBase.hpp
    	TSFCoreSerialVectorSpaceBaseDecl.hpp
    	TSFCoreSerialVectorSpaceDecl.hpp
    	TSFCoreSerialVectorSpaceFactory.hpp
    	TSFCoreSerialVectorSpaceFactoryDecl.hpp TSFCoreTypes.hpp
    	TSFCoreVector.hpp TSFCoreVectorDecl.hpp TSFCoreVectorSpace.hpp
    	TSFCoreVectorSpaceDecl.hpp TSFCoreVectorSpaceFactory.hpp
    	TSFCoreVectorSpaceFactoryDecl.hpp TSFCoreVectorStdOps.hpp
    	TSFCoreVectorStdOpsDecl.hpp TSFCore_apply_op_helper.hpp
    	TSFCore_apply_op_helper_decl.hpp
    ----------------------------------------------------------------------
    Bug 392
    Removing the old versions of the TSFCore files.

--- packages/TSFCore/src/TSFCoreVectorSpaceFactory.hpp
+++
@@ -1,9 +0,0 @@
-// ///////////////////////////////////////////////////////////////
-// VectorSpaceFactory.hpp
-
-#ifndef TSFCORE_VECTOR_SPACE_FACTORY_HPP
-#define TSFCORE_VECTOR_SPACE_FACTORY_HPP
-
-#include "TSFCoreVectorSpaceFactoryDecl.hpp"
-
-#endif  // TSFCORE_VECTOR_SPACE_FACTORY_HPP
