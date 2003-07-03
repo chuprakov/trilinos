fde861b4f1b037eaf8d87a996906d984eaabb167
rrshutt <rrshutt>
2003-07-03 18:11:35 +0000

    Added diag.h as a header file for the cleaned up diagonal prec example.

---
+++ packages/meros/src/diag.h
@@ -0,0 +1,40 @@
+#include "TSFMPI.h"
+#include "TSFOut.h"
+#include "TSFVectorType.h"
+#include "TSFProductSpace.h"
+#include "TSFBlockLinearOperator.h"
+#include "PetraVectorType.h"
+#include "PetraVectorSpace.h"
+#include "PetraVector.h"
+#include "PetraMatrix.h"
+#include "GMRESSolver.h"
+#include "AZTECSolver.h"
+#include "TSFPreconditionerFactory.h"
+#include "GenericRightPreconditioner.h"
+#include "ILUKRightPreconditionerFactory.h"
+#include "TSFPreconditioner.h"
+#include "TSFMatrixOperator.h"
+#include "Epetra_Vector.h"
+
+#ifdef EPETRA_MPI
+#include "Epetra_MpiComm.h"
+#else
+#include "Epetra_SerialComm.h"
+#endif
+
+#include "Vbr2Petra.h"
+#include "ReadPetra.h"
+#include "TSFLinearOperator2EpetraRowMatrix.h"
+#include "NSBlockPreconditionerFactory.h"
+#include "SchurFactory.h"
+#include "SchurFactoryBase.h"
+#include "DiagSchurFactory.h"
+#include "DiagRightOperatorSource.h"
+#include "GMRESSolver.h"
+#include "Aztec2TSF.h"
+
+#include "ml_include.h"
+#include "ml_epetra_operator.h"
+#include "ml_epetra_utils.h"
+
+#include "TSFIdentityOperator.h"
