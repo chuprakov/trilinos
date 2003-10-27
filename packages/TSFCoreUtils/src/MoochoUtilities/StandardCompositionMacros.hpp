e91a292ac7376cde6b13c9cf88c2a0b804a5b4eb
Roscoe A. Bartlett <rabartl@sandia.gov>
2003-10-27 22:38:35 +0000

    Checking in update to file that I forgot to check in before.  Sorry everyone.

--- packages/TSFCoreUtils/src/MoochoUtilities/StandardCompositionMacros.hpp
+++ packages/TSFCoreUtils/src/MoochoUtilities/StandardCompositionMacros.hpp
@@ -22,6 +22,9 @@
 /** Macro that adds <<std comp>> members for a composition association.
   * \ingroup StandardContainmentMacros_grp
   *
+  * This form is for when the object being held will have const attributes
+  * the same as the <tt>this</tt> object.
+  *
   * For example, if you want to include a <<std comp>> association
   * with an non-const object of type MyClass of the name my_object you
   * would include the macro in the public section of YourClass
@@ -62,9 +65,58 @@ private:															\
 	NAME ## _ptr_t	NAME ## _;										\
 public:
 
+
 ///
 /** Macro that adds <<std comp>> members for a composition association.
   * \ingroup StandardContainmentMacros_grp
+  *
+  * This form is for when the object being held will have non-const attributes
+  * irrespective of the const of <tt>this</tt>.
+  *
+  * For example, if you want to include a <<std comp>> association
+  * with an non-const object of type MyClass of the name my_object you
+  * would include the macro in the public section of YourClass
+  * declaration as follows:
+  *
+  \verbatim
+	class YourClass {
+	public:
+		STANDARD_NONCONST_COMPOSITION_MEMBERS( MyClass, my_object )
+	};
+  \endverbatim
+  *
+  * Note that the macro adds the following type and data members
+  * to the class declaration:<br>
+  \verbatim
+	public:
+		MemMngPack::ref_count_ptr<TYPE> NAME_ptr_t;
+	private:
+		NAME_ptr_t NAME_;		
+  \endverbatim
+  *
+  * Note that since this macro defines a public type it must be
+  * included before any other declarations that may need to refere to
+  * the type NAME_ptr_t (such as constuctors)
+  */
+#define STANDARD_NONCONST_COMPOSITION_MEMBERS( TYPE, NAME ) \
+	typedef MemMngPack::ref_count_ptr<TYPE> \
+		NAME ## _ptr_t; \
+	void set_ ## NAME (const NAME ## _ptr_t& NAME ) \
+	{	NAME ## _ = NAME ; } \
+	const NAME ## _ptr_t& get_ ## NAME() const \
+	{	return NAME ## _; } \
+	TYPE& NAME() const \
+	{	return *NAME ## _; } \
+private: \
+	NAME ## _ptr_t	NAME ## _; \
+public:
+
+///
+/** Macro that adds <<std comp>> members for a composition association.
+  * \ingroup StandardContainmentMacros_grp
+  *
+  * This form is for when the object being held will have const attributes
+  * irrespective of the const of <tt>this</tt>.
   *
   * For example, if you want to include a <<std comp>> association
   * with a const object of type MyClass of the name my_object you
