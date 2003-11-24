ed2f91553d62ca431349afef78055c278326fb82
Roscoe A. Bartlett <rabartl@sandia.gov>
2003-11-24 18:59:25 +0000

    Temporarly commented out some of the more expensive matrices so that the test script can run them.  I the future, we should put in a flag to run these tests for the weekly testing.

--- packages/TSFCore/example/Solvers/test-gmres.pl
+++ packages/TSFCore/example/Solvers/test-gmres.pl
@@ -7,10 +7,11 @@ $result = system ('./TSFCoreSolversGmresTest.exe ../../../../../packages/belos/t
 $success = 0 if ($result!=0);
 $result = system ('./TSFCoreSolversGmresTest.exe ../../../../../packages/belos/test/BlockGmres/fidap036.hb 1500 1e-10 -v');
 $success = 0 if ($result!=0);
-$result = system ('./TSFCoreSolversGmresTest.exe ../../../../../packages/belos/test/BlockCG/bcsstk14.hb 1200 1e-10 -v');
-$success = 0 if ($result!=0);
-$result = system ('./TSFCoreSolversGmresTest.exe ../../../../../packages/belos/test/BlockCG/bcsstk15.hb 2100 1e-10 -v');
-$success = 0 if ($result!=0);
+# RAB: 2003/11/21: Commented out because they are very expensive (todo: ad flag for including them back)
+#$result = system ('./TSFCoreSolversGmresTest.exe ../../../../../packages/belos/test/BlockCG/bcsstk14.hb 1200 1e-10 -v');
+#$success = 0 if ($result!=0);
+#$result = system ('./TSFCoreSolversGmresTest.exe ../../../../../packages/belos/test/BlockCG/bcsstk15.hb 2100 1e-10 -v');
+#$success = 0 if ($result!=0);
 if($success) {
   printf "\nFinal: Congratulations! all of the linear systems where solved to the given tolerances!\n";
 }
