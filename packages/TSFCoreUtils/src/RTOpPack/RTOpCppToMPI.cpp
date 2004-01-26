174a8c91b5a7cbaf624e3bc53fbfa86c0683244a
Roscoe A. Bartlett <rabartl@sandia.gov>
2004-01-26 22:14:05 +0000

    Tried to fix for AbstractLinAlgPack::VectorSpaceTester but this does not seem to work yet (with a parallel TSFCore-based NLP.

--- packages/TSFCoreUtils/src/RTOpPack/RTOpCppToMPI.cpp
+++ packages/TSFCoreUtils/src/RTOpPack/RTOpCppToMPI.cpp
@@ -32,23 +32,27 @@ void RTOpPack::MPI_apply_op(
 	int k;
 
 	wsp::Workspace<RTOp_SubVector> c_sub_vecs(wss,num_vecs);
-	for( k = 0; k < num_vecs; ++k ) {
-		const SubVector& v = sub_vecs[k];
-		RTOp_sub_vector(v.globalOffset(),v.subDim(),v.values(),v.stride(),&c_sub_vecs[k]);
+	if(sub_vecs) {
+		for( k = 0; k < num_vecs; ++k ) {
+			const SubVector& v = sub_vecs[k];
+			RTOp_sub_vector(v.globalOffset(),v.subDim(),v.values(),v.stride(),&c_sub_vecs[k]);
+		}
 	}
 
 	wsp::Workspace<RTOp_MutableSubVector> c_targ_sub_vecs(wss,num_targ_vecs);
-	for( k = 0; k < num_targ_vecs; ++k ) {
-		const MutableSubVector& v = targ_sub_vecs[k];
-		RTOp_mutable_sub_vector(v.globalOffset(),v.subDim(),v.values(),v.stride(),&c_targ_sub_vecs[k]);
+	if(targ_sub_vecs) {
+		for( k = 0; k < num_targ_vecs; ++k ) {
+			const MutableSubVector& v = targ_sub_vecs[k];
+			RTOp_mutable_sub_vector(v.globalOffset(),v.subDim(),v.values(),v.stride(),&c_targ_sub_vecs[k]);
+		}
 	}
 
 	RTOpCppToC op_c(op);
 	RTOp_ReductTarget reduct_objs[] = { reduct_obj }; 
 	RTOp_MPI_apply_op(
 		comm,&op_c.op_c(),root_rank,1
-		,num_vecs,num_vecs?&c_sub_vecs[0]:NULL
-		,num_targ_vecs,num_targ_vecs?&c_targ_sub_vecs[0]:NULL
+		,num_vecs, num_vecs && sub_vecs ? &c_sub_vecs[0] : NULL
+		,num_targ_vecs, num_targ_vecs && targ_sub_vecs ? &c_targ_sub_vecs[0] : NULL
 		,reduct_obj != RTOp_REDUCT_OBJ_NULL ? reduct_objs : NULL
 		);
 }
@@ -67,26 +71,31 @@ void RTOpPack::MPI_apply_op(
 	int k, j, off;
 
 	wsp::Workspace<RTOp_SubVector> c_sub_vecs(wss,num_multi_vecs*num_cols);
-	for( off = 0, j = 0; j < num_cols; ++j ) {
-		for( k = 0; k < num_multi_vecs; ++k ) {
-			const SubMultiVector& mv = sub_multi_vecs[k];
-			RTOp_sub_vector(mv.globalOffset(),mv.subDim(),&mv(1,j+1),1,&c_sub_vecs[off++]);
+	if(sub_multi_vecs) {
+		for( off = 0, j = 0; j < num_cols; ++j ) {
+			for( k = 0; k < num_multi_vecs; ++k ) {
+				const SubMultiVector& mv = sub_multi_vecs[k];
+				RTOp_sub_vector(mv.globalOffset(),mv.subDim(),&mv(1,j+1),1,&c_sub_vecs[off++]);
+			}
 		}
 	}
 
 	wsp::Workspace<RTOp_MutableSubVector> c_targ_sub_vecs(wss,num_targ_multi_vecs*num_cols);
-	for( off = 0, j = 0; j < num_cols; ++j ) {
-		for( k = 0; k < num_targ_multi_vecs; ++k ) {
-			const MutableSubMultiVector& mv = targ_sub_multi_vecs[k];
-			RTOp_mutable_sub_vector(mv.globalOffset(),mv.subDim(),&mv(1,j+1),1,&c_targ_sub_vecs[off++]);
+	if(targ_sub_multi_vecs) {
+		for( off = 0, j = 0; j < num_cols; ++j ) {
+			for( k = 0; k < num_targ_multi_vecs; ++k ) {
+				const MutableSubMultiVector& mv = targ_sub_multi_vecs[k];
+				RTOp_mutable_sub_vector(mv.globalOffset(),mv.subDim(),&mv(1,j+1),1,&c_targ_sub_vecs[off++]);
+			}
 		}
 	}
 
 	RTOpCppToC op_c(op);
 	RTOp_MPI_apply_op(
 		comm,&op_c.op_c(),root_rank,num_cols
-		,num_multi_vecs,num_multi_vecs?&c_sub_vecs[0]:NULL
-		,num_targ_multi_vecs,num_targ_multi_vecs?&c_targ_sub_vecs[0]:NULL
+		,num_multi_vecs, num_multi_vecs && sub_multi_vecs ? &c_sub_vecs[0] : NULL
+		,num_targ_multi_vecs, num_targ_multi_vecs && targ_sub_multi_vecs ? &c_targ_sub_vecs[0] : NULL
 		,reduct_objs
 		);
+
 }
