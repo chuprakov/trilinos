becc8ea1629c61fed1dc8b92de6210264d58d5d5
Kevin R. Long <kevin.long@ttu.edu>
2004-04-20 20:45:37 +0000

    changed the matrix build so that it is a test of TSF's building a matrix by summing in new values.

--- packages/TSF/examples/Tests/cgTest.cpp
+++ packages/TSF/examples/Tests/cgTest.cpp
@@ -2,30 +2,78 @@
 #include "TSF.h"
 #include <iostream>
 #include <fstream>
+#include <vector>
 
-// Michael Boldt
-// June 5, 2002
+using namespace TSF;
+using namespace std;
 
-// Solve Ax = b using CG with TSF
 int main(int argc, void** argv)
 {
   TSFMPI::init(&argc, &argv);
 
-  ofstream fout("cgTest.csv");
-
   try
     {
-      const char matrixFile [] = "matrices/tenbyten.m";
-      const double tol = 1.0e-10;
+      int n = 5;
+      TSFVectorType epetra = new PetraVectorType();
+      TSFVectorSpace space = epetra.createSpace(n);
+      TSFLinearOperator A = epetra.createMatrix(space, space);
+      TSFMatrixView M = A;
+      
+      vector<int> ncols(n);
+      vector<vector<int> > colIndices(n);
+      vector<const int*> colIndicesPtrs(n);
+      vector<vector<double> > Avals(n);
 
-      TSFVectorType vt = new DenseSerialVectorType();
+      for (int row=0; row<n; row++)
+        {
+          if (row==0)
+            {
+              ncols[row] = 2;
+              colIndices[row].resize(2);
+              colIndices[row][0] = row;
+              colIndices[row][1] = row+1;
+              Avals[row].resize(2);
+              Avals[row][0] = 2.0;
+              Avals[row][1] = -1.0;
+            }
+          else if (row==(n-1))
+            {
+              ncols[row] = 2;
+              colIndices[row].resize(2);
+              colIndices[row][0] = row;
+              colIndices[row][1] = row-1;
+              Avals[row].resize(2);
+              Avals[row][0] = 2.0;
+              Avals[row][1] = -1.0;
+            }
+          else
+            {
+              ncols[row] = 3;
+              colIndices[row].resize(3);
+              Avals[row].resize(3);
+              colIndices[row][0] = row-1;
+              colIndices[row][1] = row;
+              colIndices[row][2] = row+1;
+              Avals[row][0] = -1.0;
+              Avals[row][1] = 2.0;
+              Avals[row][2] = -1.0;
+            }
+          colIndicesPtrs[row] = &(colIndices[row][0]);
+        }
 
-      // Read A from Matrix file
-      cout << "here\n";
-      TSFMatrixReader reader = new MatlabReader(matrixFile);
-      cout << "here\n";
-      TSFLinearOperator A = reader.read(vt);
-      cout << "here\n";
+      M.setGraph(n, &(ncols[0]), &(colIndicesPtrs[0]));
+
+      M.freezeStructure();
+
+      M.zero();
+
+      for (int row=0; row<n; row++)
+        {
+          M.addToRow(row, ncols[row], &(colIndices[row][0]), &(Avals[row][0]));
+        }
+
+      M.freezeValues();
+      cerr << "A = " << endl << A << endl;
 
       // Create a known solution to check computed solution
       TSFVector soln = A.domain().createMember();
@@ -39,26 +87,27 @@ int main(int argc, void** argv)
       TSFVector p = r.copy();
 
       // Do CG
+      double convTol = 1.0e-12;
       int i = 0;
-      while (r.norm2() > tol)
-	{
+      while (r.norm2() > convTol)
+        {
 	  
-	  cerr << "iteration: " << ++i << endl;
+          cerr << "iteration: " << ++i << endl;
 	  
-	  TSFReal r2 = r * r;
-	  TSFReal pAp = p*(A*p);
+          TSFReal r2 = r * r;
+          TSFReal pAp = p*(A*p);
 	  
-	  if(TSFUtils::chop(pAp) == 0)
-	    {
-	      TSFError::raise("failure in CG");
-	    }
-	  TSFReal alpha = r2 / pAp;
+          if(TSFUtils::chop(pAp) == 0)
+            {
+              TSFError::raise("failure in CG");
+            }
+          TSFReal alpha = r2 / pAp;
 
-	  x = x + alpha * p;
-	  r = r - alpha * A * p;
-	  TSFReal beta = (r * r) / r2;
-	  p = r + beta * p;
-	};
+          x = x + alpha * p;
+          r = r - alpha * A * p;
+          TSFReal beta = (r * r) / r2;
+          p = r + beta * p;
+        };
       
       // Print results
       cerr << "RHS: " << endl << b << endl;
@@ -68,22 +117,22 @@ int main(int argc, void** argv)
       TSFReal error = (x-soln).norm2();
       cerr << "error norm = " << error << endl;
 
+      double tol = 1.0e-10;
       if(error < tol)
-	{
-	  fout << " PASSED" << endl;
-	}
+        {
+          cerr << " PASSED" << endl;
+        }
       else
-	{
-	  fout << " FAILED error>tolerance" << endl;
-	}
+        {
+          cerr << " FAILED error>tolerance" << endl;
+        }
     }
   catch(exception& e)
     {
       cerr << e.what() << endl;
-      fout << " EXCEPTION" << endl;
+      cerr << " EXCEPTION" << endl;
     }
   
-  fout.close();
 
   TSFMPI::finalize();
 }
