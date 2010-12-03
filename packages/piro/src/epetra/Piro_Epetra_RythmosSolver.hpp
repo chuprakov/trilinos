bfed98955e8f7c10969c1e38194ee7341fe9ae4b
Andy Salinger <agsalin@sandia.gov>
2010-12-03 11:45:22 -0700

    Removed unnecessary MPI_Comm typedef for serial builds

--- packages/piro/src/epetra/Piro_Epetra_RythmosSolver.hpp
+++ packages/piro/src/epetra/Piro_Epetra_RythmosSolver.hpp
@@ -28,20 +28,13 @@
 // ************************************************************************
 // @HEADER
 
-#ifndef PIRO_EPETRA_LOCASOLVER_H
-#define PIRO_EPETRA_LOCASOLVER_H
+#ifndef PIRO_EPETRA_RYTHMOSSOLVER_H
+#define PIRO_EPETRA_RYTHMOSSOLVER_H
 
 #include <iostream>
 
-#include "LOCA.H"
-#include "LOCA_Epetra.H"
 #include "Epetra_Vector.h"
 #include "Epetra_LocalMap.h"
-#include "LOCA_Epetra_ModelEvaluatorInterface.H"
-#include "LOCA_SaveEigenData_AbstractStrategy.H"
-
-#include <NOX_Epetra_MultiVector.H>
-#include <NOX_Epetra_Observer.H>
 
 #ifdef HAVE_MPI
 #include "Epetra_MpiComm.h"
@@ -50,6 +43,11 @@
 #endif
 
 #include "EpetraExt_ModelEvaluator.h"
+
+#include "Rythmos_DefaultIntegrator.hpp"
+#include "Rythmos_IntegrationObserverBase.hpp"
+#include "Rythmos_TimeStepNonlinearSolver.hpp"
+
 
 /** \brief Epetra-based Model Evaluator subclass for Charon!
  *
@@ -63,26 +61,26 @@
 namespace Piro {
 namespace Epetra {
 
-class LOCASolver
+class RythmosSolver
     : public EpetraExt::ModelEvaluator
 {
+
+  typedef double Scalar;
+
   public:
 
   /** \name Constructors/initializers */
   //@{
 
   /** \brief Takes the number of elements in the discretization . */
-  LOCASolver(Teuchos::RCP<Teuchos::ParameterList> piroParams,
-            Teuchos::RCP<EpetraExt::ModelEvaluator> model,
-            Teuchos::RCP<NOX::Epetra::Observer> observer = Teuchos::null,
-            Teuchos::RCP<LOCA::SaveEigenData::AbstractStrategy> saveEigData = Teuchos::null,
-            Teuchos::RCP<LOCA::StatusTest::Abstract> locaStatusTest_ = Teuchos::null
-            );
-
+  RythmosSolver(Teuchos::RCP<Teuchos::ParameterList> appParams,
+                Teuchos::RCP<EpetraExt::ModelEvaluator> model,
+                Teuchos::RCP<Rythmos::IntegrationObserverBase<Scalar> > observer = Teuchos::null
+                );
 
   //@}
 
-  ~LOCASolver();
+  ~RythmosSolver();
 
 
   /** \name Overridden from EpetraExt::ModelEvaluator . */
@@ -99,7 +97,7 @@ class LOCASolver
   EpetraExt::ModelEvaluator::OutArgs createOutArgs() const;
   /** \brief . */
   void evalModel( const InArgs& inArgs, const OutArgs& outArgs ) const;
-  
+
   private:
   /** \brief . */
   Teuchos::RCP<const Epetra_Map> get_x_map() const;
@@ -110,44 +108,32 @@ class LOCASolver
   /** \brief . */
   Teuchos::RCP<const Epetra_Map> get_p_map(int l) const;
   /** \brief . */
-  void setProblemParamDefaults(Teuchos::ParameterList* piroParams_);
+  void setProblemParamDefaults(Teuchos::ParameterList* appParams_);
   /** \brief . */
-  void setSolverParamDefaults(Teuchos::ParameterList* piroParams_);
+  void setSolverParamDefaults(Teuchos::ParameterList* appParams_);
+  /** \brief . */
+  Teuchos::RCP<const Teuchos::ParameterList>
+    getValidRythmosParameters() const;
 
   //@}
-  
-  
-  public:
-  
-  /** \brief \c const getter for the LOCA stepper. Can be used to retrieve statistics. */
-  Teuchos::RCP<const LOCA::Stepper> getLOCAStepper() const;
-  /** \brief Nonconst getter for the LOCA stepper. Can be used to adapt the stepper configuration while running. */
-  Teuchos::RCP<LOCA::Stepper> getLOCAStepperNonConst();
-  
-  /** \brief \c const getter for the \c LOCA::GlobalData object.*/
-  Teuchos::RCP<const LOCA::GlobalData> getGlobalData() const;
-  /** \brief Nonconst getter for the \c LOCA::GlobalData object.*/
-  Teuchos::RCP<LOCA::GlobalData> getGlobalDataNonConst();
 
   private:
 
    //These are set in the constructor and used in evalModel
-   mutable Teuchos::RCP<Teuchos::ParameterList> piroParams;
+   mutable Teuchos::RCP<Teuchos::ParameterList> appParams;
    Teuchos::RCP<EpetraExt::ModelEvaluator> model;
-   Teuchos::RCP<NOX::Epetra::Observer> observer;
-   Teuchos::RCP<LOCA::SaveEigenData::AbstractStrategy> saveEigData;
-   Teuchos::RCP<LOCA::StatusTest::Abstract> locaStatusTest;
-   NOX::Utils utils;
 
-   Teuchos::RCP<LOCA::Stepper> stepper;
-   Teuchos::RCP<NOX::Epetra::Vector> currentSolution;
-   Teuchos::RCP<LOCA::Epetra::ModelEvaluatorInterface> interface;
-   Teuchos::RCP<LOCA::ParameterVector> pVector;
    int num_p;
    int num_g;
 
-   Teuchos::RCP<LOCA::Epetra::Group> grp;
-   Teuchos::RCP<LOCA::GlobalData> globalData;
+   Teuchos::RCP<Rythmos::StepperBase<Scalar> > fwdStateStepper;
+   Teuchos::RCP<Teuchos::FancyOStream> out;
+   Teuchos::EVerbosityLevel solnVerbLevel;
+   Teuchos::RCP<Rythmos::DefaultIntegrator<Scalar> > fwdStateIntegrator;
+   Teuchos::RCP<Thyra::ModelEvaluator<double> > fwdStateModel;
+   Teuchos::RCP<Rythmos::TimeStepNonlinearSolver<double> > fwdTimeStepSolver;
+   Scalar t_final;
+
 };
 
 }
