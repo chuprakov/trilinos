GTkorvo/nnti

commit 845ee55a9b49ea379ed1af5f69ec2b8608b698a7

fixes to remove compiler warnings
Pointer variable was being used for both data and function.

packages/zoltan/src/Utilities/DDirectory/DD.h
@@ -92,7 +92,8 @@ struct Zoltan_DD_Struct {
   int debug_level;        /* Determines actions to multiple updates */
 
   DD_Hash_fn *hash;       /* Hash function used by this DD */
-  void *hashdata;         /* Data useful to compute hash */
+  void *hashdata;
+  ZOLTAN_HASH_FN *hashfn;  /* hash function */
   DD_Cleanup_fn *cleanup;
 
   MPI_Comm comm;          /* Dup of original MPI Comm (KDD)         */
@@ -152,7 +153,7 @@ typedef struct  {          /* Only used by Zoltan_DD_Remove()      */
 /***********  Distributed Directory Function Prototypes ************/
 
 unsigned int Zoltan_DD_Hash2(ZOLTAN_ID_PTR key, int num_id_entries,
- unsigned int n, void* hashdata);
+ unsigned int n, void *hashdata);
 
 void Zoltan_DD_default_cleanup(void *hashdata);
 

packages/zoltan/src/Utilities/DDirectory/DD_Hash2.c
@@ -59,8 +59,9 @@ extern "C" {
 
 #define ZOLTAN_DD_HASH_CONSTANT 2654435761U   /* consider 516595003U */
 
-unsigned int Zoltan_DD_Hash2(ZOLTAN_ID_PTR key, int num_id_entries, unsigned int n, void* hashdata)
-{
+unsigned int Zoltan_DD_Hash2(ZOLTAN_ID_PTR key, int num_id_entries, unsigned int n, 
+                           void *hashdata)
+ {
   unsigned int h, rest, *p, bytes, num_bytes;
   char *byteptr;
 
@@ -89,9 +90,9 @@ unsigned int Zoltan_DD_Hash2(ZOLTAN_ID_PTR key, int num_id_entries, unsigned int
 }
 
 
-void Zoltan_DD_default_cleanup (void *hashdata)
+void Zoltan_DD_default_cleanup (void * hashdata)
 {
-  ZOLTAN_FREE(&hashdata);
+ ZOLTAN_FREE(&hashdata);
 }
 
 #ifdef __cplusplus

packages/zoltan/src/Utilities/DDirectory/DD_Set_Hash_Fn.c
@@ -31,9 +31,9 @@ extern "C" {
 static unsigned int dd_hash_user (
   ZOLTAN_ID_PTR gid, int gid_length, 
   unsigned int nproc,
-  unsigned int (*hashdata) (ZOLTAN_ID_PTR, int, unsigned int)) 
+  ZOLTAN_HASH_FN *fn)
 {
-  return (*hashdata)(gid, gid_length, nproc);
+  return (*fn)(gid, gid_length, nproc);
 }
 
 /*************  Zoltan_DD_Set_Hash_Fn()  ***********************/
@@ -41,7 +41,7 @@ static unsigned int dd_hash_user (
 
 int Zoltan_DD_Set_Hash_Fn (
  Zoltan_DD_Directory *dd,              /* directory state information */
- unsigned int (*hash) (ZOLTAN_ID_PTR, int, unsigned int))
+ ZOLTAN_HASH_FN *hash)
 {
    char *yo = "Zoltan_DD_Set_Hash_Fn";
 
@@ -52,7 +52,7 @@ int Zoltan_DD_Set_Hash_Fn (
    }
 
    dd->hash = (DD_Hash_fn*)dd_hash_user;
-   dd->hashdata = (char *)hash;
+   dd->hashfn = hash;
    dd->cleanup = (DD_Cleanup_fn*) NULL; /* We don't have to free the function pointer */
 
    if (dd->debug_level > 0)

packages/zoltan/src/Utilities/DDirectory/DD_Update.c
@@ -230,7 +230,7 @@ static int DD_Update_Local (Zoltan_DD_Directory *dd,
       ZOLTAN_TRACE_IN (dd->my_proc, yo, NULL);
 
    /* compute offset into hash table to find head of linked list */
-   index = Zoltan_DD_Hash2 (gid, dd->gid_length, dd->table_length, dd->hashdata);
+   index = Zoltan_DD_Hash2 (gid, dd->gid_length, dd->table_length, dd->hashfn);
 
    /* walk linked list until end looking for matching gid */
    for (ptr = dd->table+index; *ptr != NULL; ptr = &((*ptr)->next))

packages/zoltan/src/Utilities/DDirectory/DD_Create.c
@@ -116,7 +116,8 @@ int Zoltan_DD_Create (
    (*dd)->table_length     = size;         /* # of linked list heads    */
    (*dd)->user_data_length = user_length;  /* optional user data length */
    (*dd)->hash             = Zoltan_DD_Hash2;/* default hash algorithm   */
-   (*dd)->hashdata         = NULL;         /* no data */
+   (*dd)->hashdata         = NULL;         /* no hash data */
+   (*dd)->hashfn         = NULL;         /* no hash function */
    (*dd)->cleanup          = NULL;         /* user registered cleanup   */
    (*dd)->max_id_length    = (num_gid > num_lid) ? num_gid : num_lid;
 

chuprakov/trilinos

commit 7806c76ae64a599f05303cd635df7691e8a2b3d6

Fix DD user-supplied hash function bug
This is a bug I introduced in 845ee55a9b49ea3.

I fixed the bug, and also added code to zdrive that
tests the use of a user-supplied hash function.  I
added "Test DDirectory = 1" to a ch_simple test.

packages/zoltan/src/Utilities/DDirectory/DD.h
@@ -21,8 +21,8 @@
 #include "zoltan_id.h"
 #include "zoltan_util.h"
 #include "zoltan_dd.h"
-
 #ifdef __cplusplus
+
 /* if C++, define the rest of this header file as extern C */
 extern "C" {
 #endif
@@ -64,7 +64,8 @@ typedef struct DD_Node  {
 
 /* Hash function */
 
-typedef unsigned int DD_Hash_fn (ZOLTAN_ID_PTR, int, unsigned int, void *);
+typedef unsigned int DD_Hash_fn (ZOLTAN_ID_PTR, int, unsigned int, void *,
+                                 ZOLTAN_HASH_FN *);
 typedef void DD_Cleanup_fn (void*);
 
 /* The directory structure, Zoltan_DD_Struct, is created by the call
@@ -91,10 +92,10 @@ struct Zoltan_DD_Struct {
   int remove_msg_size;    /* Total allocation for DD_REMOVE_MSG     */
   int debug_level;        /* Determines actions to multiple updates */
 
-  DD_Hash_fn *hash;       /* Hash function used by this DD */
-  void *hashdata;
-  ZOLTAN_HASH_FN *hashfn;  /* hash function */
-  DD_Cleanup_fn *cleanup;
+  DD_Hash_fn *hash;       /* Hash function used by this DD         */
+  void *hashdata;         /* Either our hash data ...              */
+  ZOLTAN_HASH_FN *hashfn; /* ... Or user's hash function, not both */
+  DD_Cleanup_fn *cleanup; /* Functioned to free our hash data      */
 
   MPI_Comm comm;          /* Dup of original MPI Comm (KDD)         */
   DD_Node *table[1];      /* Hash table, heads of the link lists    */
@@ -153,7 +154,7 @@ typedef struct  {          /* Only used by Zoltan_DD_Remove()      */
 /***********  Distributed Directory Function Prototypes ************/
 
 unsigned int Zoltan_DD_Hash2(ZOLTAN_ID_PTR key, int num_id_entries,
- unsigned int n, void *hashdata);
+ unsigned int n, void *hashdata, ZOLTAN_HASH_FN *fn);
 
 void Zoltan_DD_default_cleanup(void *hashdata);
 

packages/zoltan/src/Utilities/DDirectory/DD_Find.c
@@ -121,7 +121,7 @@ int Zoltan_DD_Find (
 
    /* for each GID, fill DD_Find_Msg buffer and contact list */
    for (i = 0; i < count; i++)  {
-      procs[i] = dd->hash (gid + i*dd->gid_length, dd->gid_length, dd->nproc, dd->hashdata);
+      procs[i] = dd->hash (gid + i*dd->gid_length, dd->gid_length, dd->nproc, dd->hashdata, dd->hashfn);
       ptr      = (DD_Find_Msg*) (sbuff + i * dd->find_msg_size);
 
       ptr->index = i;
@@ -249,8 +249,7 @@ static int DD_Find_Local (Zoltan_DD_Directory *dd,
       ZOLTAN_TRACE_IN (dd->my_proc, yo, NULL);
 
    /* compute offset into hash table to find head of linked list */
-   index = Zoltan_DD_Hash2 (gid, dd->gid_length, dd->table_length, dd->hashdata);
-
+   index = Zoltan_DD_Hash2 (gid, dd->gid_length, dd->table_length, dd->hashdata, NULL);
    /* walk link list until end looking for matching global ID */
    for (ptr = dd->table[index]; ptr != NULL; ptr = ptr->next)
       if (ZOLTAN_EQ_ID (dd->gid_length, gid, ptr->gid) == TRUE)  { 

packages/zoltan/src/Utilities/DDirectory/DD_Hash2.c
@@ -60,7 +60,7 @@ extern "C" {
 #define ZOLTAN_DD_HASH_CONSTANT 2654435761U   /* consider 516595003U */
 
 unsigned int Zoltan_DD_Hash2(ZOLTAN_ID_PTR key, int num_id_entries, unsigned int n, 
-                           void *hashdata)
+                           void *hashdata, ZOLTAN_HASH_FN *fn)
  {
   unsigned int h, rest, *p, bytes, num_bytes;
   char *byteptr;

packages/zoltan/src/Utilities/DDirectory/DD_Remove.c
@@ -91,8 +91,7 @@ int Zoltan_DD_Remove (
 
    /* for each GID, fill in contact list and then message structure */
    for (i = 0; i < count; i++)  {
-      procs[i] = dd->hash(gid + i*dd->gid_length, dd->gid_length, dd->nproc, dd->hashdata);
-
+      procs[i] = dd->hash(gid + i*dd->gid_length, dd->gid_length, dd->nproc, dd->hashdata, dd->hashfn);
       ptr = (DD_Remove_Msg*) (sbuff + i * dd->remove_msg_size);
       ptr->owner = dd->my_proc;
       ZOLTAN_SET_ID (dd->gid_length, ptr->gid, gid + i * dd->gid_length);
@@ -188,7 +187,7 @@ static int DD_Remove_Local (Zoltan_DD_Directory *dd,
       ZOLTAN_TRACE_IN (dd->my_proc, yo, NULL);
 
    /* compute offset into hash table to find head of linked list */
-   index = Zoltan_DD_Hash2 (gid, dd->gid_length, dd->table_length, dd->hashdata);
+   index = Zoltan_DD_Hash2 (gid, dd->gid_length, dd->table_length, dd->hashdata, NULL);
 
    /* walk linked list until end looking for matching gid (key) */
    for (ptr = dd->table + index; *ptr != NULL; ptr = &((*ptr)->next))

packages/zoltan/src/Utilities/DDirectory/DD_Set_Hash_Fn.c
@@ -31,6 +31,7 @@ extern "C" {
 static unsigned int dd_hash_user (
   ZOLTAN_ID_PTR gid, int gid_length, 
   unsigned int nproc,
+  void *data,
   ZOLTAN_HASH_FN *fn)
 {
   return (*fn)(gid, gid_length, nproc);
@@ -52,8 +53,9 @@ int Zoltan_DD_Set_Hash_Fn (
    }
 
    dd->hash = (DD_Hash_fn*)dd_hash_user;
+   dd->hashdata = NULL;
    dd->hashfn = hash;
-   dd->cleanup = (DD_Cleanup_fn*) NULL; /* We don't have to free the function pointer */
+   dd->cleanup = (DD_Cleanup_fn*) NULL; 
 
    if (dd->debug_level > 0)
       ZOLTAN_PRINT_INFO (dd->my_proc, yo, "Successful");

packages/zoltan/src/Utilities/DDirectory/DD_Set_Neighbor_Hash_Fn1.c
@@ -31,7 +31,7 @@ struct dd_nh1_struct {
 };
 
 static unsigned int dd_nh1 (ZOLTAN_ID_PTR gid, int gid_length,
- unsigned int nproc, struct dd_nh1_struct* hashdata) ;
+ unsigned int nproc, struct dd_nh1_struct* hashdata, ZOLTAN_HASH_FN *fn) ;
 
 
 
@@ -69,8 +69,9 @@ int Zoltan_DD_Set_Neighbor_Hash_Fn1 (
 
    hashdata->groupsize   = size;
    dd->hash    = (DD_Hash_fn*) &dd_nh1;
-   dd->cleanup = (DD_Cleanup_fn*) &Zoltan_DD_default_cleanup;
+   dd->hashfn  = NULL;
    dd->hashdata = hashdata;
+   dd->cleanup = (DD_Cleanup_fn*) &Zoltan_DD_default_cleanup;
    hashdata->max_gid = size * dd->nproc;     /* larger GIDs out of range */
 
    return ZOLTAN_OK;
@@ -79,7 +80,7 @@ int Zoltan_DD_Set_Neighbor_Hash_Fn1 (
 
 
 static unsigned int dd_nh1 (ZOLTAN_ID_PTR gid, int gid_length,
- unsigned int nproc, struct dd_nh1_struct* hashdata)
+ unsigned int nproc, struct dd_nh1_struct* hashdata, ZOLTAN_HASH_FN *fn)
    {
    int id = (signed) *gid;
    return (id < hashdata->max_gid) ? (id / hashdata->groupsize) : (id % nproc);

packages/zoltan/src/Utilities/DDirectory/DD_Set_Neighbor_Hash_Fn2.c
@@ -43,7 +43,7 @@ struct dd_nh2_struct {
 };
 
 static unsigned int dd_nh2 (ZOLTAN_ID_PTR gid, int gid_length,
- unsigned int nproc, struct dd_nh2_struct* hashdata);
+ unsigned int nproc, struct dd_nh2_struct* hashdata, ZOLTAN_HASH_FN *fn);
 
 static int compare_sort   (const void *a, const void *b);
 static int compare_search (const void *a, const void *b);
@@ -90,8 +90,9 @@ int Zoltan_DD_Set_Neighbor_Hash_Fn2 (
 
    /* register functions for automatic invocation */
    dd->hash    = (DD_Hash_fn*) &dd_nh2;
-   dd->cleanup = (DD_Cleanup_fn*)&dd_nh2_cleanup;
    dd->hashdata = hashdata;
+   dd->hashfn    = NULL;
+   dd->cleanup = (DD_Cleanup_fn*)&dd_nh2_cleanup;
 
    /* malloc and initialize storage for range information structures */
    hashdata->ptr = (Range_Info*)  ZOLTAN_MALLOC (n * sizeof (Range_Info));
@@ -120,7 +121,7 @@ int Zoltan_DD_Set_Neighbor_Hash_Fn2 (
 
 
 static unsigned int dd_nh2 (ZOLTAN_ID_PTR gid, int gid_length,
- unsigned int junk, struct dd_nh2_struct* hashdata)
+ unsigned int junk, struct dd_nh2_struct* hashdata, ZOLTAN_HASH_FN *fn)
    {
    Range_Info *p;
    char *yo = "dd_ny2";

packages/zoltan/src/Utilities/DDirectory/DD_Set_Neighbor_Hash_Fn3.c
@@ -33,7 +33,7 @@ struct dd_nh3_struct {
 
 
 static unsigned int dd_nh3 (ZOLTAN_ID_PTR gid, int gid_length,
- unsigned int nproc, struct dd_nh3_struct* hashdata);
+ unsigned int nproc, struct dd_nh3_struct* hashdata, ZOLTAN_HASH_FN *);
 
 
 
@@ -73,6 +73,7 @@ int Zoltan_DD_Set_Neighbor_Hash_Fn3 (
 
   dd->hash    = (DD_Hash_fn*) &dd_nh3;
   dd->hashdata    = hashdata;
+  dd->hashfn  = NULL;
   dd->cleanup = (DD_Cleanup_fn*)&Zoltan_DD_default_cleanup;
 
   return ZOLTAN_OK;
@@ -80,7 +81,7 @@ int Zoltan_DD_Set_Neighbor_Hash_Fn3 (
 
 
 static unsigned int dd_nh3 (ZOLTAN_ID_PTR gid, int gid_length,
- unsigned int nproc, struct dd_nh3_struct * hashdata)
+ unsigned int nproc, struct dd_nh3_struct * hashdata, ZOLTAN_HASH_FN *fn)
 {
   int id = (signed) *gid;
   if (id < hashdata->breakpt)

packages/zoltan/src/Utilities/DDirectory/DD_Update.c
@@ -97,7 +97,7 @@ int Zoltan_DD_Update (
 
    /* for each GID given, fill in contact list and then message structure */
    for (i = 0; i < count; i++)  {
-      procs[i] = dd->hash(gid + i*dd->gid_length, dd->gid_length, dd->nproc, dd->hashdata);
+      procs[i] = dd->hash(gid + i*dd->gid_length, dd->gid_length, dd->nproc, dd->hashdata, dd->hashfn);
       ptr      = (DD_Update_Msg*) (sbuff + i * dd->update_msg_size);
 
       ptr->lid_flag       = (lid)  ? 1 : 0;
@@ -230,7 +230,7 @@ static int DD_Update_Local (Zoltan_DD_Directory *dd,
       ZOLTAN_TRACE_IN (dd->my_proc, yo, NULL);
 
    /* compute offset into hash table to find head of linked list */
-   index = Zoltan_DD_Hash2 (gid, dd->gid_length, dd->table_length, dd->hashfn);
+   index = Zoltan_DD_Hash2 (gid, dd->gid_length, dd->table_length, dd->hashdata, NULL);
 
    /* walk linked list until end looking for matching gid */
    for (ptr = dd->table+index; *ptr != NULL; ptr = &((*ptr)->next))

packages/zoltan/src/driver/dr_maps.c
@@ -44,6 +44,11 @@ static void sort_and_compare_maps(int, int, MESH_INFO_PTR,
 /*****************************************************************************/
 /*****************************************************************************/
 
+unsigned int zdrive_hash(ZOLTAN_ID_TYPE *id, int ngid, unsigned int nprocs)
+{
+  return (id[0] % nprocs);
+}
+
 /*****************************************************************************/
 /*****************************************************************************/
 /*****************************************************************************/
@@ -344,19 +349,12 @@ ZOLTAN_COMM_OBJ *comm, *comm_copy;
    */
 
   MPI_Allreduce(&num_elems, &max_nelems, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+
   ierr = Zoltan_DD_Create(&dd, MPI_COMM_WORLD, 1, 1, 0, max_nelems, 0);
   if (ierr) {
     Gen_Error(0, "Fatal:  Error returned by Zoltan_DD_Create");
     error = 1;
   }
-
-  ierr = Zoltan_DD_Update(dd, gids, lids, NULL, NULL, num_elems);
-  if (ierr) {
-    Gen_Error(0, "Fatal:  Error returned by Zoltan_DD_Update");
-    error = 1;
-  }
-
-  free(gids);
 
   /*
    * Test DD copy functions
@@ -376,7 +374,25 @@ ZOLTAN_COMM_OBJ *comm, *comm_copy;
     error = 1;
   }
 
-  Zoltan_DD_Destroy(&ddCopy);
+  /*
+   * Test the use of a user provided hash function in one copy.
+   */
+
+  Zoltan_DD_Set_Hash_Fn(ddCopy, zdrive_hash);
+
+  ierr = Zoltan_DD_Update(dd, gids, lids, NULL, NULL, num_elems);
+  if (ierr) {
+    Gen_Error(0, "Fatal:  Error returned by Zoltan_DD_Update of dd");
+    error = 1;
+  }
+
+  ierr = Zoltan_DD_Update(ddCopy, gids, lids, NULL, NULL, num_elems);
+  if (ierr) {
+    Gen_Error(0, "Fatal:  Error returned by Zoltan_DD_Update ddCopy");
+    error = 1;
+  }
+
+  free(gids);
 
   /* 
    * Use the DDirectory to find owners of off-processor neighboring elements. 
@@ -419,11 +435,19 @@ ZOLTAN_COMM_OBJ *comm, *comm_copy;
   ierr = Zoltan_DD_Find(dd, nbor_gids, nbor_lids,
                         NULL, NULL, num_nbor, ownerlist);
   if (ierr) {
-    Gen_Error(0, "Fatal:  Error returned by Zoltan_DD_Find");
+    Gen_Error(0, "Fatal:  Error returned by Zoltan_DD_Find of dd");
+    error = 1;
+  }
+
+  ierr = Zoltan_DD_Find(ddCopy, nbor_gids, nbor_lids,
+                        NULL, NULL, num_nbor, ownerlist);
+  if (ierr) {
+    Gen_Error(0, "Fatal:  Error returned by Zoltan_DD_Find of ddCopy");
     error = 1;
   }
  
   Zoltan_DD_Destroy(&dd);
+  Zoltan_DD_Destroy(&ddCopy);
 
   /*
    * Check for errors 

packages/zoltan/test/ch_simple/zdrive.inp.rcb
@@ -6,3 +6,4 @@ File Name		= simple
 Parallel Disk Info	= number=0
 Test Drops		= 1
 Test RCB Box		= 1
+Test DDirectory = 1