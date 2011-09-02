16fae028761b7ae6bdd92d75143542f60d4eb267
Kevin Long <kevin.long@ttu.edu>
2011-09-01 19:27:32 -0500

    Moved MPIComm and associated classes from Teuchos to Playa.

--- packages/Sundance/Playa/src/PlayaErrorPolling.hpp
+++ packages/Sundance/Playa/src/PlayaErrorPolling.hpp
@@ -1,46 +1,8 @@
 // @HEADER
-// ***********************************************************************
-//
-//                    Teuchos: Common Tools Package
-//                 Copyright (2004) Sandia Corporation
-//
-// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
-// license for use of this work by or on behalf of the U.S. Government.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-// 1. Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright
-// notice, this list of conditions and the following disclaimer in the
-// documentation and/or other materials provided with the distribution.
-//
-// 3. Neither the name of the Corporation nor the names of the
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
-//
-// ***********************************************************************
 // @HEADER
 
-#ifndef TEUCHOS_ERRORPOLLING_H
-#define TEUCHOS_ERRORPOLLING_H
+#ifndef Playa_ERRORPOLLING_H
+#define Playa_ERRORPOLLING_H
 
 #include "Teuchos_ConfigDefs.hpp"
 #include "Teuchos_TestForException.hpp"
@@ -49,7 +11,7 @@
 */
 //@{
 
-namespace Teuchos
+namespace Playa
 {
   class MPIComm;
 
@@ -118,7 +80,7 @@ namespace Teuchos
    * @param comm [in] The communicator on which polling will be done
    */
 #define TEUCHOS_POLL_FOR_FAILURES(comm)                                  \
-  TEST_FOR_EXCEPTION(Teuchos::ErrorPolling::pollForFailures(comm), \
+  TEST_FOR_EXCEPTION(Playa::ErrorPolling::pollForFailures(comm), \
                      std::runtime_error,                                     \
                      "off-processor error detected by proc=" << (comm).getRank());
 }
--- packages/Sundance/Playa/src/PlayaMPIContainerComm.hpp
+++ packages/Sundance/Playa/src/PlayaMPIContainerComm.hpp
@@ -1,58 +1,20 @@
 // @HEADER
-// ***********************************************************************
-//
-//                    Teuchos: Common Tools Package
-//                 Copyright (2004) Sandia Corporation
-//
-// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
-// license for use of this work by or on behalf of the U.S. Government.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-// 1. Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright
-// notice, this list of conditions and the following disclaimer in the
-// documentation and/or other materials provided with the distribution.
-//
-// 3. Neither the name of the Corporation nor the names of the
-// contributors may be used to endorse or promote products derived from
-// this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
-//
-// ***********************************************************************
+
 // @HEADER
 
-#ifndef TEUCHOS_MPICONTAINERCOMM_H
-#define TEUCHOS_MPICONTAINERCOMM_H
+#ifndef PLAYA_MPICONTAINERCOMM_H
+#define PLAYA_MPICONTAINERCOMM_H
 
-/*! \file Teuchos_MPIContainerComm.hpp
+/*! \file PlayaMPIContainerComm.hpp
   \brief Object representation of an MPI communicator for templated containers
 */
 
-#include "Teuchos_ConfigDefs.hpp"
-#include "Teuchos_Array.hpp"
-#include "Teuchos_MPIComm.hpp"
-#include "Teuchos_MPITraits.hpp"
+#include "PlayaMPIComm.hpp"
+#include "PlayaMPITraits.hpp"
 
-namespace Teuchos
+namespace Playa
 {
+using Teuchos::Array;
   /** \ingroup MPI
    * \brief Object representation of an MPI communicator for templated containers
    * \note Template specialization exists for <tt>std::string</tt>.
